97c027f9afef582eea7f5fdbab182f6176c85ff8
Ollie <ollierwoodman@gmail.com>
2025-04-01 11:22:57 +0800

    Use SVG favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,7 @@ export const metadata: Metadata = {
       { url: '/apple-icon-180x180.png', sizes: '180x180' },
     ],
     icon: [
-      { url: '/android-icon-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon.svg', sizes: 'any', type: 'image/svg+xml' },
     ],
     other: [
       { rel: 'manifest', url: '/manifest.json' },
